27149f61af6c5de9739a5c61087794b04a3f3e0c
Daniel Ventura de Almeida <danielvalmeida91@gmail.com>
2026-07-09 22:10:13 -0300

    feat: finalizando erros na tela de cadastro de produtos.

--- frontend/src/pages/produtos/novo.tsx
+++ frontend/src/pages/produtos/novo.tsx
@@ -3,6 +3,11 @@ import { AxiosError } from "axios";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
+interface IErrorFields {
+  field: string,
+  message: string
+}
+
 export default function NovoProduto() {
   // TODO: um useState para cada campo do produto (nome, descricao, preco, quantidade)
   const router = useRouter();
@@ -10,10 +15,33 @@ export default function NovoProduto() {
   const [descricao, setDescricao] = useState("");
   const [quantidade, setQuantidade] = useState("");
   const [preco, setPreco] = useState("");
+  const [fieldErrors, setFieldErrors] = useState({
+    nome: '',
+    descricao: '',
+    quantidade: '',
+    preco: ''
+  })
+
+  const handleResetFieldErrors = () => {
+    setFieldErrors({
+      nome: '',
+      descricao: '',
+      quantidade: '',
+      preco: ''
+    })
+  }
+
+  const user = {
+    age: 35,
+    name: 'Daniel'
+  }
+
+  console.log('nome', user['name'])
 
   const handleSubmit = async (e: React.SubmitEvent) => {
     e.preventDefault();
     // TODO: POST /produtos com os dados do formulário
+    handleResetFieldErrors();
     const body = {
       nome,
       descricao,
@@ -25,7 +53,11 @@ export default function NovoProduto() {
       console.log('update', createProduct.data)
       router.push('/produtos')
     } catch (error) {
-      if (error instanceof AxiosError) return console.warn(error?.response?.data)
+      if (error instanceof AxiosError) {
+        const fields: IErrorFields[] = error.response?.data.fields
+        fields.map(aux => setFieldErrors(prev => ({ ...prev, [aux.field]: aux.message })))
+        return console.warn(error?.response?.data)
+      }
     }
     // TODO: redirecionar para /produtos após sucesso (useRouter)
   };
@@ -33,17 +65,29 @@ export default function NovoProduto() {
   return (
     <form onSubmit={handleSubmit} className="w-full h-screen flex flex-col items-center justify-center">
       {/* TODO: inputs controlados para nome, descricao, preco e quantidade */}
-      <label htmlFor="">Nome do produto: </label>
-      <input className="px-1 py-2 min-w-24 border border-zinc-400 rounded-md mb-5" type="text" name="nome" onChange={(e) => setNome(e.target.value)} value={nome} />
+      <div className="flex flex-col justify-center relative mb-5">
+        <label htmlFor="">Nome do produto: </label>
+        <input className={`px-1 py-2 min-w-24 border ${fieldErrors.nome.length > 0 ? 'border-red-500' : 'border-zinc-400'} rounded-md`} type="text" name="nome" onChange={(e) => setNome(e.target.value)} value={nome} />
+        <span className="text-sm text-red-500 -mb-21 absolute">{fieldErrors.nome}</span>
+      </div>
 
-      <label htmlFor="">Descrição do produto: </label>
-      <input className="px-1 py-2 min-w-24 border border-zinc-400 rounded-md mb-5" type="text" name="descricao" onChange={(e) => setDescricao(e.target.value)} value={descricao} />
+      <div className="flex flex-col justify-center relative mb-5">
+        <label htmlFor="">Descrição do produto: </label>
+        <input className={`px-1 py-2 min-w-24 border ${fieldErrors.descricao.length > 0 ? 'border-red-500' : 'border-zinc-400'} rounded-md`} type="text" name="descricao" onChange={(e) => setDescricao(e.target.value)} value={descricao} />
+        <span className="text-sm text-red-500 -mb-21 absolute">{fieldErrors.descricao}</span>
+      </div>
+      <div className="flex flex-col justify-center relative mb-5">
+        <label htmlFor="">Quantidade do produto: </label>
+        <input className={`px-1 py-2 min-w-24 border ${fieldErrors.quantidade.length > 0 ? 'border-red-500' : 'border-zinc-400'} rounded-md`} type="text" name="quantidade" onChange={(e) => setQuantidade(e.target.value)} value={quantidade} />
+        <span className="text-sm text-red-500 -mb-21 absolute">{fieldErrors.quantidade}</span>
+      </div>
+      <div className="flex flex-col justify-center relative mb-5">
+        <label htmlFor="">Preço do produto: </label>
+        <input className={`px-1 py-2 min-w-24 border ${fieldErrors.preco.length > 0 ? 'border-red-500' : 'border-zinc-400'} rounded-md`} type="text" name="preco" onChange={(e) => setPreco(e.target.value)} value={preco} />
+        <span className="text-sm text-red-500 -mb-21 absolute">{fieldErrors.preco}</span>
+      </div>
 
-      <label htmlFor="">Quantidade do produto: </label>
-      <input className="px-1 py-2 min-w-24 border border-zinc-400 rounded-md mb-5" type="text" name="quantidade" onChange={(e) => setQuantidade(e.target.value)} value={quantidade} />
 
-      <label htmlFor="">Preço do produto: </label>
-      <input className="px-1 py-2 min-w-24 border border-zinc-400 rounded-md mb-5" type="text" name="preco" onChange={(e) => setPreco(e.target.value)} value={preco} />
       {/* TODO: botão de submit */}
       <button className="p-4 min-w-18 border border-zinc-300 bg-sky-700 text-white rounded-sm">Cadastrar</button>
     </form>
